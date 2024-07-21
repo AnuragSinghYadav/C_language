130eac85bf4501dee3f4b6d1de8ce14267d1713c
Anurag Singh Yadav <117581504+AnuragSinghYadav@users.noreply.github.com>
2024-07-21 21:07:52 +0530

    Add files via upload

---
+++ Fractionalpart.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float x = 5.7;
+    int y = x;
+    printf("Enter the decimal no :%d\n",y);
+    float z =x-y;
+    printf("your fractional part is:%f",z);
+    return 0;
+}   
