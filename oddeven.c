8ca4b8a5a60dcd4e804aaf67a3d3b71ca11c4c96
Anurag Singh Yadav <117581504+AnuragSinghYadav@users.noreply.github.com>
2024-07-21 21:15:09 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a no");
+    scanf("%d",&n);
+    if (n%2==0)  
+    {
+      printf("Even Number");  
+    }
+    else{
+        printf("ODD number");
+    }
+      
+      return 0;
+    
+}
